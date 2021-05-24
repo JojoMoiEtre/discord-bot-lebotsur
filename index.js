40e6e59b2950075a4361831a812d175b672baffd
JojoMoiEtre <79185789+JojoMoiEtre@users.noreply.github.com>
2021-05-25 00:41:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js');
 const Puissance4 = require('./Puissance4');
 const Pendu = require('./Pendu');
 const Fast = require('./Fast');
+const Help = require('./Help');
 
 const bot = new Discord.Client();
 const puissance4 = new Puissance4(bot);
 const pendu = new Pendu(bot);
 const fast = new Fast(bot);
+const help = new Help(bot);
 
 var prefix = '<';
 var blague_123soleil = 1;
@@ -99,30 +101,16 @@ bot.on('message', async message => {
         else if (niv === '5') {
             fast.newGame(message, niv);
         }
+        else if (niv === '#') {
+            fast.newGame(message, niv);
+        }
+        else if (niv === '##') {
+            fast.newGame(message, niv);
+        }
     }
     // Help
     else if (message.content.toLowerCase() === prefix + 'help') {
-        const embed = new Discord.MessageEmbed()
-            .setColor('#fc2eff')
-            .setTitle('Help - Commandes')
-            .setDescription("Pour le moment je ne sais pas faire grand chose (parce que jojo se touche les couilles au lieu de m'ajouter des fonctionnalités)\n")
-            .addField("Jeux","**" + prefix + "fast `niveau`** - C'est un jeu où vous devez retaper la chaîne de caractères choisie par mes soins le plus rapidement possible"
-                                +"\n*• Niveau **1*** : 15 lettres minuscules"
-                                +"\n*• Niveau **2*** : 15 lettres minuscules ou majuscules"
-                                +"\n*• Niveau **3*** : 15 lettres minuscules (avec ou sans accent), majuscules ou caractères numériques"
-                                +"\n*• Niveau **4*** : 15 lettres minuscules (avec ou sans accent), majuscules, caractères numériques ou spéciaux"
-                                +"\n*• Niveau **5*** : 25 lettres minuscules (avec ou sans accent), majuscules, caractères numériques ou spéciaux"
-                            +"\n**" + prefix + "pendu** - Si t’es vraiment con où conne (pas de sexisme ici) il s'agit du jeu du pendu, le truc où il faut deviner un mot sinon il y a un Jean-Michel qui meurt\n#RIPJeanMichel"
-                            +"\n**" + prefix + "puissance4** - Normalement tu sais lire mais au cas où t'ai un QI inférieur à 10, cette commande permet de lancer une partie de Puissance 4"
-                            , false)
-            .addField("Tout savoir sur...", "**" + prefix + "bot** - Tout savoir sur le bot génial que je suis"
-                            +"\n**" + prefix + "jojo** - Tout savoir sur sur notre magnifique Jojo"
-                            +"\n**" + prefix + "paudrey** - Tout savoir sur notre magnifique Paudrey"
-                            , false)
-            .addField("Utile", "**" + prefix + "roll `nombre`** - Je choisis une valeur entre 0 et le nombre que tu as indiqué"
-                            , false)
-            .setTimestamp();
-        message.channel.send(embed);
+        help.execute(message);
     }
 });
 
